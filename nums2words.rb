6cdda2cd1b5261bda99e7a13d3d5f09bb6d83f07
Denys Triasunov <dendden@users.noreply.github.com>
2015-07-09 17:52:34 +0300

    version 3.0
    This update raises the limit of spoken words to trillions

--- nums2words.rb
+++ nums2words.rb
@@ -7,25 +7,44 @@ def num2words num
     end
 
     numString = ''
+    bigNames = ['trillion','billion','million','thousand','hundred']
     onesPlace = ['one','two','three','four','five','six','seven','eight','nine']
     tensPlace = ['ten','twenty','thirty','fourty','fifty','sixty','seventy','eighty','ninety']
     teens = ['eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen']
 
     left = num
-    write = left / 100
-    left = left % 100
+    divisor = 1000000000000
+    bigNameIndex = 0
+    while ( divisor > 1000 && left < divisor )
+        divisor /= 1000
+        bigNameIndex += 1
+    end
+
+    if left < divisor
+        divisor = 100
+        bigNameIndex += 1
+    end
+
+    write = left / divisor
+    left %= divisor
 
     if write > 0
-        hundreds = num2words write
-        numString += hundreds + ' hundred'
+        bigName = num2words write
+        numString += bigName + ' ' + bigNames[bigNameIndex]
 
         if left > 0
             numString += ' '
         end
     end
 
-    write = left / 10
-    left = left % 10
+    if left < 100
+        write = left / 10
+        left = left % 10
+    else
+        write = left
+        numString += num2words write
+        return numString
+    end
 
     if write > 0
         if ( write == 1 && left > 0 )
